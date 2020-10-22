c526f38b7799d32716428a7313b5d56dd1eb2aa1
Your Name <kyawtkyawtsan1998@gmail.com>
2020-10-22 20:51:12 +0630

    error on di

--- app/src/main/java/com/kyawt/baganmap/di/ViewModelFactory.kt
+++ app/src/main/java/com/kyawt/baganmap/di/ViewModelFactory.kt
@@ -1,26 +1,26 @@
 package com.kyawt.baganmap.di
-
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import javax.inject.Inject
-import javax.inject.Provider
-import javax.inject.Singleton
-
-
-@Singleton
-class ViewModelFactory @Inject constructor(
-    private val viewModels: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
-) : ViewModelProvider.Factory {
-
-    @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val creator = viewModels[modelClass]
-            ?: viewModels.asIterable().firstOrNull { modelClass.isAssignableFrom(it.key) }?.value
-        requireNotNull(creator) { "unknown model class $modelClass" }
-        return try {
-            creator.get() as T
-        } catch (e: Exception) {
-            throw RuntimeException(e)
-        }
-    }
-}
+//
+//import androidx.lifecycle.ViewModel
+//import androidx.lifecycle.ViewModelProvider
+//import javax.inject.Inject
+//import javax.inject.Provider
+//import javax.inject.Singleton
+//
+//
+//@Singleton
+//class ViewModelFactory @Inject constructor(
+//    private val viewModels: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+//) : ViewModelProvider.Factory {
+//
+//    @Suppress("UNCHECKED_CAST")
+//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//        val creator = viewModels[modelClass]
+//            ?: viewModels.asIterable().firstOrNull { modelClass.isAssignableFrom(it.key) }?.value
+//        requireNotNull(creator) { "unknown model class $modelClass" }
+//        return try {
+//            creator.get() as T
+//        } catch (e: Exception) {
+//            throw RuntimeException(e)
+//        }
+//    }
+//}
--- app/src/main/java/com/kyawt/baganmap/di/module/NetworkModule.kt
+++ app/src/main/java/com/kyawt/baganmap/di/module/NetworkModule.kt
@@ -1,40 +1,40 @@
 package com.kyawt.baganmap.di.module
 
-import com.kyawt.baganmap.utils.SharePreferenceHelper
-import com.kyawt.baganmap.utils.network.interceptor.RequestInterceptor
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
-@Module
-@InstallIn(ApplicationComponent::class)
-object NetworkModule {
-
-    @Provides
-    fun provideRequestInterceptor(pref: SharePreferenceHelper): RequestInterceptor {
-        return RequestInterceptor(pref)
-    }
-
-    @Provides
-    fun provideOkHttpClient(requestInterceptor: RequestInterceptor): OkHttpClient {
-        return OkHttpClient
-            .Builder()
-            .addNetworkInterceptor(requestInterceptor)
-            .build()
-    }
-
-    @Provides
-    fun provideRetrofit(client: OkHttpClient): Retrofit.Builder {
-        return Retrofit
-            .Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
-
-    }
+//import com.kyawt.baganmap.utils.SharePreferenceHelper
+//import com.kyawt.baganmap.utils.network.interceptor.RequestInterceptor
+//import dagger.Module
+//import dagger.Provides
+//import dagger.hilt.InstallIn
+//import dagger.hilt.android.components.ApplicationComponent
+//import okhttp3.OkHttpClient
+//import retrofit2.Retrofit
+//import retrofit2.converter.gson.GsonConverterFactory
+//
+//@Module
+//@InstallIn(ApplicationComponent::class)
+//object NetworkModule {
+//
+//    @Provides
+//    fun provideRequestInterceptor(pref: SharePreferenceHelper): RequestInterceptor {
+//        return RequestInterceptor(pref)
+//    }
+//
+//    @Provides
+//    fun provideOkHttpClient(requestInterceptor: RequestInterceptor): OkHttpClient {
+//        return OkHttpClient
+//            .Builder()
+//            .addNetworkInterceptor(requestInterceptor)
+//            .build()
+//    }
+//
+//    @Provides
+//    fun provideRetrofit(client: OkHttpClient): Retrofit.Builder {
+//        return Retrofit
+//            .Builder()
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .client(client)
+//
+//    }
 
 //    @Provides
 //    fun provideMuseumAPI(retrofit: Retrofit.Builder): MuseumAPI {
@@ -43,4 +43,4 @@ object NetworkModule {
 //            .build()
 //            .create(MuseumAPI::class.java)
 //    }
-}
+//}
